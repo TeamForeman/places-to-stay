918e7146cc9f9e297e090edee7002e86eb4709fa
Parker Stafford <parker.stafford92@gmail.com>
2020-11-12 11:51:48 -0800

    Added commented out descriptions of each object that will be added to the relatedListings array in listings table and the favorites array in users table in DB.

--- db/db.js
+++ db/db.js
@@ -9,10 +9,31 @@ const listingSchema = new mongoose.Schema({
   relatedListings: Array
 });
 
+// each related listing in the related listings array will look like this:
+// realted = {
+//   id: Number, // for adding to favorites
+//   type: String, // eg., house, appartment, condo
+//   numOfBeds: Number,
+//   photo_url: String,
+//   superhost: Boolean,
+//   rating: Number, // if 0 new listing or no ratings
+//   num_of_ratings: Number, // if 0 new listing or no ratings
+//   description: String,
+//   price: Number
+//   }
+
 const userSchema = new mongoose.Schema({
   id: {type: Number, unique: true},
   favorites: Array
 });
+
+// each list in the array of favorites will look like this:
+// favorite = {
+//   name: 'list name',
+//   photoUrl: 'photo url for the first listing in the list'
+//   listings: [/*listing id's*/]
+//  }
+
 
 const Listing = mongoose.model('Listing', listingSchema);
 const User = mongoose.model('User', userSchema);
