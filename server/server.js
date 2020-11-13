e8bf1ee857812eaf36f2205076ddc48c495fc671
Parker Stafford <parker.stafford92@gmail.com>
2020-11-12 20:21:58 -0800

    Modified db var name because models had to be exported as well.

--- server/server.js
+++ server/server.js
@@ -6,13 +6,20 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public/dist')));
 
-const port = 3000;
+const port = 3004;
+
+app.get('/1', (req, res) => {
+  // console.log(req);
+  res.data = 'hello';
+  res.location('/1');
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Server connected at http://localhost:${port}`);
 });
 
-db.on('error', console.error.bind(console, 'error'));
-db.once('open', () => {
+db.con.on('error', console.error.bind(console, 'error'));
+db.con.once('open', () => {
   console.log('Connected to beartnt!');
 });
