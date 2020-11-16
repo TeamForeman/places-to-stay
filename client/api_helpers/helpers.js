8cf7ccac3bfaf4f03792ac236624266d22c24e9f
Parker Stafford <parker.stafford92@gmail.com>
2020-11-16 12:31:48 -0800

    Made helper function to get a listing from db.

--- client/api_helpers/helpers.js
+++ client/api_helpers/helpers.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 var getListing = (id) => {
   return axios.get(`/api/more/listings/${id}`)
     .then(results => {
+      console.log(results.data.relatedListings);
       return ([results.data.lId, results.data.relatedListings]);
     })
     .catch(err => {
