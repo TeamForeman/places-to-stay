844b6862608fb6a660e5cf76552100fe2d0db554
Parker Stafford <parker.stafford92@gmail.com>
2020-11-17 15:21:28 -0800

    Styled header and modified styles for each listing.

--- client/src/styles/styled_components.js
+++ client/src/styles/styled_components.js
@@ -5,7 +5,11 @@ export const Main = styled.div`
   text-align: center;
   margin: 0 auto;
 `;
-
+export const ListingDiv = styled.div`
+  width: 20%;
+  height: 100%;
+  position: relative;
+`;
 export const SlidingDiv = styled.div`
   max-width: 1128px;
   display: flex;
@@ -47,7 +51,7 @@ export const PageButton = styled.div`
   box-sizing: border-box;
   border-radius: 50%;
   border 1px solid rgba(0, 0, 0, 0.08);
-  color: rgb(34, 34, 34);
+  color: black;
   background-color: rgba(255, 255, 255, .9);
   cursor: pointer;
   width: 32px;
@@ -60,8 +64,28 @@ export const PageButton = styled.div`
   transition: box-shadow 0.2s ease 0s, -ms-transform 0.25s ease 0s, -webkit-transform 0.25s ease 0s, transform 0.25s ease 0s !important;
   &:hover {
     transform: scale(1.04);
-    background-color: rgb(255, 255, 255);
+    background-color: white;
     box-shadow: transparent 0px 0px 0px 1px, transparent 0px 0px 0px 4px,
     rgba(0, 0, 0, .12) 0px 6px 16px;
   }
+`;
+
+export const SuperHeart = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  padding: 8px;
+`;
+
+export const Super = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  background-color: rgba(255, 255, 255, .95);
+  border: .5px solid rgba(0, 0, 0, 0.2);
+  color: black;
+  border-radius: 4px;
+  padding: 4px 8px;
+  box-shadow: rgba(0, 0, 0, .18) 0px 2px 4px;
 `;
