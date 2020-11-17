c6a1cd73d4725bba1e5f02c11825207279636162
Parker Stafford <parker.stafford92@gmail.com>
2020-11-16 21:09:24 -0800

    Implemented next and previous functions that slide the related listings on button click.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -24,10 +24,33 @@ const App = () => {
   }, []);
 
   var next = () => {
-
+    if (!window.location.hash) {
+      window.location.href = window.location.href + '#2';
+    } else if (window.location.hash === '#2') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#3';
+    } else if (window.location.hash === '#3') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#1';
+    } else if (window.location.hash === '#1') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#2';
+    }
   };
 
   var previous = () => {
+    if (!window.location.hash) {
+      window.location.href = window.location.href + '#3';
+    } else if (window.location.hash === '#2') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#1';
+    } else if (window.location.hash === '#3') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#2';
+    } else if (window.location.hash === '#1') {
+      var sliced = window.location.href.slice(0, window.location.href.length - 2);
+      window.location.href = sliced + '#3';
+    }
 
   };
 
@@ -35,12 +58,12 @@ const App = () => {
     <React.Fragment>
       <div>
         <h2>More places to stay</h2>
-        <button type="button" onClick={next}>
+        <button type="button" onClick={previous}>
           <svg viewBox="0 0 18 18">
             <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path>
           </svg>
         </button>
-        <button type="button" onClick={previous}>
+        <button type="button" onClick={next}>
           <svg viewBox="0 0 18 18">
             <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fill-rule="evenodd"></path>
           </svg>
