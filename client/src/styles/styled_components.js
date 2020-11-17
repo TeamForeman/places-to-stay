a4da0c66c4fb9d3d50bb26de4815766b42112f8a
Parker Stafford <parker.stafford92@gmail.com>
2020-11-16 16:13:18 -0800

    Began styling components.

--- client/src/styles/styled_components.js
+++ client/src/styles/styled_components.js
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
 
+export const FlexUl = styled.ul`
+  max-width: 1128px;
+  display: flex;
+  overflow-x: scroll;
+  sroll-behavior: smooth;,
+  scroll-snap-type: x mandatory;
+  text-align: center;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
 export const FlexDiv = styled.div`
   display: flex;
+  text-align: center;
   justify-content: space-between;
+  margin: 0 auto;
 `;
