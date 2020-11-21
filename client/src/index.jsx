7bf68ba4b54690ca06408df2705bab6bc1655713
Parker Stafford <parker.stafford92@gmail.com>
2020-11-21 12:35:28 -0800

    Add conditional rendering to so that component renders on proxy or in service development.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+if (document.getElementById('app')) {
+  ReactDOM.render(<App/>, document.getElementById('app'));
+} else {
+  ReactDOM.render(<App/>, document.getElementById('service4'));
+}
