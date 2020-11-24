17157d0f92185bb9b97161f662f82e8c039e46ec
Parker Stafford <parker.stafford92@gmail.com>
2020-11-23 17:15:24 -0800

    Format and style component and set-up handle change function for form.

--- client/src/components/CreateList.jsx
+++ client/src/components/CreateList.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {ModalDiv, Overlay, ModalHead, CloseButton, ModalTitle, AllFavs, CreateDiv, ModalFoot} from '../styles/styled_components';
+import {ModalDiv, Overlay, ModalHead, CloseButton, ModalTitle, ModalFoot, AddListButton, ListInput, InputWrap, FormWrap, CharLimit, ListLabel, NameDiv, InnerName} from '../styles/styled_components';
+
 
 var CreateList = (props) => {
+
+  const [input, setInput] = useState(true);
+  const [disabled, setDisabled] = useState(true);
+
   if (!props.showing) {
     return null;
   }
 
+  var handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.value === '') {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+    setInput(e.target.value);
+  };
+
   return ReactDOM.createPortal(
     <React.Fragment>
-      <Overlay onClick={props.closeFunc}>
-        <ModalDiv>
-          <ModalHead>
-            <CloseButton onClick={props.closeFunc}>X</CloseButton>
-            <ModalTitle>Name This List</ModalTitle>
-            <div />
-          </ModalHead>
-        </ModalDiv>
-      </Overlay>
+      <Overlay onClick={props.closeFunc} />
+      <ModalDiv>
+        <ModalHead>
+          <CloseButton onClick={props.closeFunc}>X</CloseButton>
+          <ModalTitle>Name This List</ModalTitle>
+          <div />
+        </ModalHead>
+        <FormWrap>
+          <ListLabel for="name-of-list">
+            <InputWrap>
+              <ListInput id="name-of-list" maxlength="50" type="text" required onChange={handleChange}></ListInput>
+              <NameDiv id="name-transform">
+                <InnerName>Name</InnerName>
+              </NameDiv>
+            </InputWrap>
+          </ListLabel>
+          <CharLimit>50 characters maximum</CharLimit>
+        </FormWrap>
+        <ModalFoot>
+          <AddListButton disabled={disabled}>Create</AddListButton>
+        </ModalFoot>
+      </ModalDiv>
     </React.Fragment>,
     document.getElementById('portal'));
 };
