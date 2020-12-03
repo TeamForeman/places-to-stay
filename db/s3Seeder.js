150d83f217d3d2bc6a73d52ae869fd1ca32fc546
Michael Wetterauer <mjw0486@gmail.com>
2020-12-03 18:17:22 -0500

    Adds S3 seeding script

---
+++ db/s3Seeder.js
@@ -0,0 +1,20 @@
+const faker = require('faker');
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+
+const url = faker.image.abstract();
+
+// Make images folder in root of this project
+
+for (let i = 1; i <= 1000; i ++) {
+  const writeStream = fs.createWriteStream(path.join(__dirname,'../images', `${i}.jpg`));
+  axios({
+    method: 'get',
+    url: url,
+    responseType: 'stream',
+  })
+  .then((response) => {
+    response.data.pipe(writeStream);
+  });
+}
